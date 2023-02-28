6ecc9dfd3ecc16f8be53e498212d2c2a8875ab88
Emi Riosmena <A01704245@tec.mx>
2023-02-28 11:06:43 -0600

    add this

---
+++ Labs/script.js
@@ -0,0 +1,25 @@
+console.log("Hello, World!");
+console.error("This is an error");
+
+const filesystem = require('fs');
+filesystem.writeFileSync('hola.txt', "Hello, World!");
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 200, 340, 1000, 50];
+for (let i of arreglo) {
+    setTimeout(() => {console.log(i)}, i);
+}
+
+console.log("Este log esta antes pero despues de imprimir el arreglo");
+setTimeout(() => {console.log("Ya te hackie")}, 12000);
+
+const http = require('http');
+const server = http.createServer((request, response) => {
+    console.log(request.url);
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<head><meta charset="utf-8"></head><body>');
+    response.write("<h1>Amogus</h1>");
+    response.write("ඞඞඞඞඞඞඞඞඞ")
+    response.end();
+});
+
+server.listen(3000);
--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-/*console.log("Hello, World!");
-console.error("This is an error");
-
-const filesystem = require('fs');
-filesystem.writeFileSync('hola.txt', "Hello, World!");
-
-const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 200, 340, 1000, 50];
-for (let i of arreglo) {
-    setTimeout(() => {console.log(i)}, i);
-}
-
-console.log("Este log esta antes pero despues de imprimir el arreglo");
-setTimeout(() => {console.log("Ya te hackie")}, 12000);
-
-const http = require('http');
-const server = http.createServer((request, response) => {
-    console.log(request.url);
-    response.setHeader('Content-Type', 'text/html');
-    response.write("<h1>Amogus</h1>");
-    response.write("ඞඞඞඞඞඞඞඞඞ")
-    response.end();
-});
-
-server.listen(3000);*/
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
